884e19941701b9b1e7c0aef7e1126a6febaabbea
kamilzienkiewicz3 <kamilzienkiewicz3@gmail.com>
2026-03-17 20:23:07 +0100

    main Update

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-img = cv2.imread("Crossroads_img.tiff")
+img = cv2.imread("StandardResolution.tiff")
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-height, width = img_rgb.shape[:2]
-pooled_image = cv2.resize(img_rgb, (width // 4, height // 4), interpolation=cv2.INTER_AREA)
+#height, width = img_rgb.shape[:2]
+#pooled_image = cv2.resize(img_rgb, (width // 4, height // 4), interpolation=cv2.INTER_AREA)
 
-crop = pooled_image[500:1000, 750:1500]
+#crop = pooled_image[500:1000, 750:1500]
 
 
 lower_rgb = np.array([160, 160, 0])   
 upper_rgb = np.array([200, 200, 200])
-mask_rgb = cv2.inRange(crop, lower_rgb, upper_rgb)
+mask_rgb = cv2.inRange(img_rgb, lower_rgb, upper_rgb)
 
 mask_inverted = cv2.bitwise_not(mask_rgb)
 
@@ -33,21 +33,21 @@ mask = cv2.inRange(img_hsv, lower_gray, upper_gray)
 
 plt.figure(figsize=(12,4))
 
-plt.subplot(1,3,1)
-plt.hist(crop[:,:,0].ravel(), bins=256)
-plt.title("Red channel")
+#plt.subplot(1,3,1)
+#plt.hist(crop[:,:,0].ravel(), bins=256)
+#plt.title("Red channel")
 
 
-plt.subplot(1,3,2)
-plt.hist(crop[:,:,1].ravel(), bins=256)
-plt.title("Green channel")
+#plt.subplot(1,3,2)
+#plt.hist(crop[:,:,1].ravel(), bins=256)
+#plt.title("Green channel")
 
 
-plt.subplot(1,3,3)
-plt.hist(crop[:,:,2].ravel(), bins=256)
-plt.title("Blue channel")
+#plt.subplot(1,3,3)
+#plt.hist(crop[:,:,2].ravel(), bins=256)
+#plt.title("Blue channel")
 
-plt.show()
+#plt.show()
 
 plt.subplot(1, 2, 1)
 plt.imshow(mask_inverted, cmap='gray')
